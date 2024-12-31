bc2144c2610e4566a2de370b99bb299adcac6014
tomoyuki <junkor-1011>
2024-12-31 22:51:19 +0900

    default hook

--- workspace/backend/src/app.ts
+++ workspace/backend/src/app.ts
@@ -3,7 +3,19 @@ import { OpenAPIHono } from '@hono/zod-openapi';
 import { registerRoutesWithOpenAPI } from './routes';
 
 export function createApp() {
-  const app = new OpenAPIHono();
+  const app = new OpenAPIHono({
+    defaultHook: (result, c) => {
+      if (!result.success) {
+        // request validation error handling
+        return c.json(
+          {
+            error: 'Invalid Request',
+          },
+          422,
+        );
+      }
+    },
+  });
 
   // routes
   registerRoutesWithOpenAPI(app);
